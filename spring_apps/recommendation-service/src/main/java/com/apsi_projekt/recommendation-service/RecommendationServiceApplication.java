fb5d08975c1f9ae803979a211936a6ebb21bd301
jtokarsk <01148812@pw.edu.pl>
2024-11-23 21:45:20 +0100

    build all services

--- spring_apps/recommendation-service/src/main/java/com/apsi_projekt/recommendation-service/RecommendationServiceApplication.java
+++ spring_apps/recommendation-service/src/main/java/com/apsi_projekt/recommendation-service/RecommendationServiceApplication.java
@@ -15,7 +15,7 @@ public class RecommendationServiceApplication {
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "recommendation-service") String name) {
-		return String.format("Hello %s!", name, "in docker now");
+		return String.format("Hello %s!", name, "in docker now!");
 	}
 
 }
