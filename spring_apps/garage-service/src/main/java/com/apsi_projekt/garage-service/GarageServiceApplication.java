e6f970458e4cf02973d0d2b861ec12958e9369a8
Aleksandra Weronika Bigda <a.bigda@stud.elka.pw.edu.pl>
2024-11-23 13:14:15 +0100

    dodanie garage-service

---
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage-service/GarageServiceApplication.java
@@ -0,0 +1,17 @@
+package com.apsi_projekt.garage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestControler
+public class GarageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageServiceApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "garage-service") String name) {
+		return String.format("Hello %s!", name);
+
+}
---
+++ spring_apps/garage-service/src/test/java/com/apsi_projekt/garage-service/GarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsi_projekt.garage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
