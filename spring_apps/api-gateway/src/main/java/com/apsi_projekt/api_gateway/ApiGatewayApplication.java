63aa28e00a9ac5127e78590535d1421f0c8e400b
jtokarsk <01148812@pw.edu.pl>
2024-11-23 13:53:48 +0100

    Update ApiGatewayApplication.java

--- spring_apps/api-gateway/src/main/java/com/apsi_projekt/api_gateway/ApiGatewayApplication.java
+++ spring_apps/api-gateway/src/main/java/com/apsi_projekt/api_gateway/ApiGatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestControler
+@RestController
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -13,5 +13,5 @@ public class ApiGatewayApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "api-gateway") String name) {
 		return String.format("Hello %s!", name);
-
+	}
 }
