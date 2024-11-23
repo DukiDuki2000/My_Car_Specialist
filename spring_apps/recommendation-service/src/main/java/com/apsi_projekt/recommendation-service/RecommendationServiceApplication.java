45f34f9d4f227cc8a0f96ec5eaf46aa38fef3195
Aleksandra Weronika Bigda <a.bigda@stud.elka.pw.edu.pl>
2024-11-23 13:33:59 +0100

    dodanie recommendation-service

---
+++ spring_apps/recommendation-service/src/main/java/com/apsi_projekt/recommendation-service/RecommendationServiceApplication.java
@@ -0,0 +1,17 @@
+package com.apsi_projekt.recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestControler
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "recommendation-service") String name) {
+		return String.format("Hello %s!", name);
+
+}
