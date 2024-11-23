4074f1851cc520829c14d305fc8c7f1ba814d80e
Aleksandra Weronika Bigda <a.bigda@stud.elka.pw.edu.pl>
2024-11-23 01:20:08 +0100

    Upload New File

---
+++ spring_apps/vechicle-service/VehicleServiceApplication.java
@@ -0,0 +1,17 @@
+package com.apsi_projekt.vehicle_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestControler
+public class VehicleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "vehicle-service") String name) {
+		return String.format("Hello %s!", name);
+
+}
