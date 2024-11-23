b4c15aea30a788ce09377e1a8231e1f5ec354f9c
jtokarsk <01148812@pw.edu.pl>
2024-11-23 13:57:41 +0100

    Update GarageServiceApplication.java

--- spring_apps/garage-service/src/main/java/com/apsi_projekt/garage-service/GarageServiceApplication.java
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage-service/GarageServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestControler
+@RestController
 public class GarageServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,5 +13,5 @@ public class GarageServiceApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "garage-service") String name) {
 		return String.format("Hello %s!", name);
-
+	}
 }
