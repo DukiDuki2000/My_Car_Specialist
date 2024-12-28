31bfc8f89c2f2aeb7378b6355e289d87db1c1fdf
kzych1 <01148822@pw.edu.pl>
2024-12-28 20:43:48 +0100

    poprawienie build.gradle.kts- dodanie commons-codec

--- spring_apps/vehicle-service/build.gradle.kts
+++ spring_apps/vehicle-service/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	//implementation("org.springframework.kafka:spring-kafka")
+	implementation("commons-codec:commons-codec")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	//runtimeOnly("org.postgresql:postgresql")
