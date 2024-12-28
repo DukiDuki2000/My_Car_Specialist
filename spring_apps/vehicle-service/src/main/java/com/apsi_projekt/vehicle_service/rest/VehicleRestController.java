dcaf64c8a021a8ce874635fd776ceceddc5da970
Kamil Z <01148822@pw.edu.pl>
2024-12-28 20:34:28 +0100

    Dodanie VIN dekodera oraz dodanie zmiennych w ci/cd api_key oraz secret_key

--- spring_apps/vehicle-service/src/main/java/com/apsi_projekt/vehicle_service/rest/VehicleRestController.java
+++ spring_apps/vehicle-service/src/main/java/com/apsi_projekt/vehicle_service/rest/VehicleRestController.java
@@ -1,6 +1,10 @@
 package com.apsi_projekt.vehicle_service.rest;
 
+import com.apsi_projekt.vehicle_service.model.VinDecodeResponse;
+import com.apsi_projekt.vehicle_service.service.VinDecoderService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("vehicle")
 public class VehicleRestController {
 
-    public VehicleRestController() {
+    private final VinDecoderService vinDecoderService;
+
+    public VehicleRestController(VinDecoderService vinDecoderService) {
+        this.vinDecoderService = vinDecoderService;
     }
 
     @GetMapping()
     public String sayHello() {
         return "Hello from Vehicle Service";
+    }
+
+
+    @GetMapping("/decode-info/{vin}")
+    public ResponseEntity<VinDecodeResponse> decodeInfo(@PathVariable("vin") String vin) {
+        VinDecodeResponse response =vinDecoderService.decode(vin);
+        return ResponseEntity.ok(response);
     }
 }
