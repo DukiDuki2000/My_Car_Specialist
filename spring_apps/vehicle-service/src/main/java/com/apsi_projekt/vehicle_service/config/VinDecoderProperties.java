5646e5c62db493b6ca2ddd4e5d4b6ff4d6fe2bc5
Kamil Z <01148822@pw.edu.pl>
2024-12-28 21:20:55 +0100

    [Proba naprawy bledu] Dodanie wypisywania apki_key i secret key

--- spring_apps/vehicle-service/src/main/java/com/apsi_projekt/vehicle_service/config/VinDecoderProperties.java
+++ spring_apps/vehicle-service/src/main/java/com/apsi_projekt/vehicle_service/config/VinDecoderProperties.java
@@ -1,6 +1,7 @@
 package com.apsi_projekt.vehicle_service.config;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +25,10 @@ public class VinDecoderProperties {
 
     public String getSecretKey() {
         return secretKey;
+    }
+    @PostConstruct
+    public void debugLog() {
+        System.out.println("[DEBUG] vin.decoder.apiKey=" + apiKey);
+        System.out.println("[DEBUG] vin.decoder.secretKey=" + secretKey);
     }
 }
