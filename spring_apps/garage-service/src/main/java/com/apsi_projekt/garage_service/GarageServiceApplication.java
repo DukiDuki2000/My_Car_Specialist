ab597015ab24f9aa6c1142ae23a0e5857dce163f
Kamil Z <01148822@pw.edu.pl>
2024-12-29 16:21:44 +0100

    Nowa funkcjonalnosc- wyciaganie inframcje z nipu

--- spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/GarageServiceApplication.java
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/GarageServiceApplication.java
@@ -2,9 +2,11 @@ package com.apsi_projekt.garage_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +18,9 @@ public class GarageServiceApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "garage-service") String name) {
 		return String.format("Hello %s!", name, "in container now!!");
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
---
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/service/GarageService.java
@@ -0,0 +1,37 @@
+package com.apsi_projekt.garage_service.service;
+
+import com.apsi_projekt.garage_service.dto.CompanyInfo;
+import com.apsi_projekt.garage_service.model.VATResposne;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+@Service
+public class GarageService {
+    private final RestTemplate restTemplate;
+    private final String apiUrl = "https://wl-api.mf.gov.pl/api/search/nip/{nip}?date={date}";
+    @Autowired
+    public GarageService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public CompanyInfo getCompanyInfoByNip(String nip) {
+        String date = LocalDate.now().toString();
+
+        VATResposne response = restTemplate.getForObject(apiUrl, VATResposne.class, nip, date);
+
+        if (response != null && response.getResult() != null && response.getResult().getSubject() != null) {
+            VATResposne.Subject subject = response.getResult().getSubject();
+            return new CompanyInfo(
+                    subject.getName(),
+                    subject.getResidenceAddress(),
+                    subject.getRegon(),
+                    subject.getNip()
+            );
+        } else {
+            throw new RuntimeException("Nie znaleziono danych dla NIP: " + nip);
+        }
+    }
+}
