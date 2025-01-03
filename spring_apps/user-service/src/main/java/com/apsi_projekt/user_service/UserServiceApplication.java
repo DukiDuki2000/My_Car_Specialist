46426ab01e180fe34c03aea763ef3bc4523d995d
Kamil Z <01148822@pw.edu.pl>
2025-01-03 17:51:06 +0100

    [TEST2] Zmiana secret key dla serwisow

--- spring_apps/user-service/src/main/java/com/apsi_projekt/user_service/UserServiceApplication.java
+++ spring_apps/user-service/src/main/java/com/apsi_projekt/user_service/UserServiceApplication.java
@@ -16,7 +16,7 @@ public class UserServiceApplication {
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "user-service") String name) {
-		return String.format("Hello %s!", name, "in docker now!!");
+		return String.format("Hello %s!", name, "in docker now!");
 	}
 
 }
