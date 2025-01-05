0a8987f96b5309dad2848e9b10293a545e05d82f
jtokarsk <01148812@pw.edu.pl>
2025-01-05 21:24:33 +0100

    CompanyInfo Lombok Update

--- spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/dto/CompanyInfo.java
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/dto/CompanyInfo.java
@@ -1,53 +1,14 @@
 package com.apsi_projekt.garage_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CompanyInfo {
     private String companyName;
     private String companyAddress;
     private String workingAddress;
     private String companyNip;
     private String companyRegon;
-
-    public CompanyInfo() {}
-
-    public CompanyInfo(String companyName, String companyAddress,String workingAddress, String companyRegon, String companyNip) {
-        this.companyName = companyName;
-        this.companyAddress = companyAddress;
-        this.workingAddress = workingAddress;
-        this.companyRegon = companyRegon;
-        this.companyNip = companyNip;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCompanyAddress() {return companyAddress;}
-
-    public void setCompanyAddress(String companyAddress) {
-        this.companyAddress = companyAddress;
-    }
-
-    public String getCompanyNip() {
-        return companyNip;
-    }
-
-    public void setCompanyNip(String companyNip) {
-        this.companyNip = companyNip;
-    }
-
-    public String getCompanyRegon() {
-        return companyRegon;
-    }
-
-    public void setCompanyRegon(String companyRegon) {
-        this.companyRegon = companyRegon;
-    }
-
-    public String getAddresOFWHAT(){
-        return (companyAddress != null && !companyAddress.isEmpty()) ? companyAddress : workingAddress;
-    }
 }
