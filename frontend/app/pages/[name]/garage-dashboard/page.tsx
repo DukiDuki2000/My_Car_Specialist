c0fd177788cfbad5620ebae940f30279adfd8f3e
kwojcik1 <01162589@pw.edu.pl>
2025-01-06 13:20:33 +0100

    Strona glowna serwis

--- frontend/app/pages/[name]/garage-dashboard/page.tsx
+++ frontend/app/pages/[name]/garage-dashboard/page.tsx
@@ -12,26 +12,26 @@ export default function GarageDashboard() {
         setIsClient(true);
     }, []);
 
-    useEffect(() => {
-        if (!isClient) return; // Zatrzymaj, jeśli nie jesteśmy po stronie klienta
+    // useEffect(() => {
+    //     if (!isClient) return; // Zatrzymaj, jeśli nie jesteśmy po stronie klienta
 
-        // Sprawdzanie, czy użytkownik jest zalogowany
-        const token = localStorage.getItem('token');
-        const username = localStorage.getItem('username');
-        const role = localStorage.getItem('role');
+    //     // Sprawdzanie, czy użytkownik jest zalogowany
+    //     const token = localStorage.getItem('token');
+    //     const username = localStorage.getItem('username');
+    //     const role = localStorage.getItem('role');
 
-        if (!token || !username || !role) {
-            // Jeśli brakuje tokena, username lub roli, przekierowanie na stronę logowania
-            router.push('/pages/auth/login');
-            return;
-        }
+    //     if (!token || !username || !role) {
+    //         // Jeśli brakuje tokena, username lub roli, przekierowanie na stronę logowania
+    //         router.push('/pages/auth/login');
+    //         return;
+    //     }
 
-        if (role !== 'ROLE_GARAGE') {
-            // Przekierowanie, jeśli użytkownik nie jest garażem
-            router.push('/pages/auth/login');
-            return;
-        }
-    }, [router, isClient]);
+    //     if (role !== 'ROLE_GARAGE') {
+    //         // Przekierowanie, jeśli użytkownik nie jest garażem
+    //         router.push('/pages/auth/login');
+    //         return;
+    //     }
+    // }, [router, isClient]);
 
     const handleNavigate = (path: string) => {
         router.push(path); // Przekierowanie na podaną ścieżkę
@@ -44,8 +44,8 @@ export default function GarageDashboard() {
     return (
         <div className="absolute bottom-0 h-100 w-full">
             {/* Treść */}
-            <main className="flex items-center justify-center flex-col -mt-16 px-4 h-[calc(100vh-80px)]">
-                <h2 className="text-4xl font-bold text-gray-800 mb-12">Panel Warsztatu</h2>
+            <main className="flex items-center justify-center flex-col -mt-16 px-4 h-[calc(80vh-80px)]">
+                <h2 className="text-4xl font-bold text-gray-800 mb-14">Panel Warsztatu</h2>
 
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
 
@@ -55,7 +55,7 @@ export default function GarageDashboard() {
                         className="flex flex-col items-center justify-center border-4 border-blue-400 rounded-lg p-16 shadow-md hover:shadow-lg hover:border-blue-600 transition transform hover:scale-110 cursor-pointer"
                     >
                         <img
-                            src="/listaZgloszen.svg"
+                            src="/note.svg"
                             alt="Lista zgłoszeń"
                             className="h-40 w-40 mb-8"
                         />
@@ -107,7 +107,7 @@ export default function GarageDashboard() {
                         className="flex flex-col items-center justify-center border-4 border-blue-400 rounded-lg p-16 shadow-md hover:shadow-lg hover:border-blue-600 transition transform hover:scale-110 cursor-pointer"
                     >
                         <img
-                            src="/listaZgloszen.svg"
+                            src="/history.svg"
                             alt="Lista zgłoszeń"
                             className="h-40 w-40 mb-8"
                         />
