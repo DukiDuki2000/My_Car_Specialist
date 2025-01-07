6e7079d873f9dce385326d626c05c9c3c89de461
Kamil Z <01148822@pw.edu.pl>
2025-01-07 22:10:05 +0100

    Dodanie zlecen serwisowych wersje 1.0

--- spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/model/Report.java
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/model/Report.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,12 +19,16 @@ public class Report {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    @Column(name="created_at", nullable=false)
-    private LocalDate createdAt;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "report_dates",
+            joinColumns = @JoinColumn(name = "report_id")
+    )
+    @Column(name = "date", nullable = false)
+    private List<@NotNull LocalDateTime> dateHistory = new ArrayList<>();
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name="garage_id",nullable=false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name="garage_id", nullable=false)
     private Garage garage;
 
     @NotNull
@@ -35,11 +41,32 @@ public class Report {
             name = "report_operations",
             joinColumns = @JoinColumn(name = "service_report_id")
     )
-    @Column(name = "operations", nullable = false)
+
+    @Column(name = "operations")
     private List<@NotBlank String> operations;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "report_operation_dates",
+            joinColumns = @JoinColumn(name = "report_id")
+    )
+    @Column(name = "operation_date", nullable = false)
+    private List<@NotNull LocalDateTime> operationDates = new ArrayList<>();
+
+    @NotNull
+    @Column(name = "vehicle_id", nullable = false)
+    private Long vehicleId;
 
     private Long userId;
     private String userName;
+
+    @Column(name="description",nullable=false)
+    private String description;
+
+    @PrePersist
+    protected void onCreate() {
+        this.dateHistory.add(LocalDateTime.now());
+    }
 }
 
 
--- spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/service/ReportService.java
+++ spring_apps/garage-service/src/main/java/com/apsi_projekt/garage_service/service/ReportService.java
@@ -6,9 +6,12 @@ import com.apsi_projekt.garage_service.repositories.ReportRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -20,41 +23,89 @@ public class ReportService {
     public Report createReport(Report report,HttpServletRequest request) {
         String usernameHeader = request.getHeader("X-Username");
         String idHeader = request.getHeader("X-Id");
-
+        report.setUserName(usernameHeader);
+        report.setUserId(Long.parseLong(idHeader));
         if (report.getStatus() == null){
             report.setStatus(ReportStatus.NEW);
         }
         return reportRepository.save(report);
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_MODERATOR','ROLE_ADMIN')")
+
     public List<Report> getAllReports() {
         return reportRepository.findAll();
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_USER')")
-    public List<Report> getAllUserReports(Report report,HttpServletRequest request) {
+
+    public List<Report> getAllUserReports(HttpServletRequest request) {
         String usernameHeader = request.getHeader("X-Username");
-        String idHeader = request.getHeader("X-Id");
-        report.setUserName(usernameHeader);
-        report.setUserId(Long.parseLong(idHeader));
         return reportRepository.findByUserName(usernameHeader);
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_GARAGE')")
-    public List<Report> getAllGarageReports(Report report){
-        return reportRepository.findByGarageId(report.getGarage().getId());
+
+    public List<Report> getAllGarageReports(HttpServletRequest request){
+        String garageId = request.getHeader("X-Id");
+        return reportRepository.findByGarageId(Long.parseLong(garageId));
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_MODERATOR','ROLE_ADMIN','ROLE_GARAGE','ROLE_USER')")
+    public List<Report> getAllStatusReports(HttpServletRequest request, String statusStr){
+        String usergarageId = request.getHeader("X-Id");
+        ReportStatus status;
+        try {
+            status = ReportStatus.valueOf(statusStr.toUpperCase().replace("-", "_"));
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid status value: " + statusStr);
+        }
+        return reportRepository.findByStatusAndUserId(status,Long.parseLong(usergarageId));
+    }
     public Report changeReportStatus(Long reportId, ReportStatus newStatus) {
         Report report = reportRepository.findById(reportId)
                 .orElseThrow(() -> new ResourceNotFoundException("Not found ID: " + reportId));
+        ReportStatus currentStatus = report.getStatus();
+        if (currentStatus == newStatus) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Report already has status: " + currentStatus);
+        }
+        switch (currentStatus) {
+            case NEW:
+                if (newStatus != ReportStatus.IN_PROGRESS) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "From NEW status, you can only transition to IN_PROGRESS.");
+                }
+                break;
+            case IN_PROGRESS:
+                if (newStatus != ReportStatus.COMPLETED) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "From IN_PROGRESS status, you can only transition to COMPLETED.");
+                }
+                break;
+            case COMPLETED:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "COMPLETED status is final and cannot be changed");
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid current status: " + currentStatus);
+        }
+
         report.setStatus(newStatus);
+        report.getDateHistory().add(LocalDateTime.now());
         return reportRepository.save(report);
     }
 
-    //Edycja
 
+    public Report addOperationsToReport(Long reportId, List<String> operations) {
+        Report report = reportRepository.findById(reportId)
+                .orElseThrow(() -> new ResourceNotFoundException("\n" +
+                        "Service report not found with ID: " + reportId));
 
+        LocalDateTime now = LocalDateTime.now();
+        for (String operation : operations) {
+            report.getOperations().add(operation);
+            report.getOperationDates().add(now);
+        }
+
+        report.getDateHistory().add(now);
+
+        return reportRepository.save(report);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_MODERATOR','ROLE_ADMIN','ROLE_GARAGE','ROLE_USER')")
+    public List<Report> getCompletedReportsByVehicleId(Long vehicleId) {
+        return reportRepository.findByStatusAndVehicleId(ReportStatus.COMPLETED,vehicleId);
+    }
 }
