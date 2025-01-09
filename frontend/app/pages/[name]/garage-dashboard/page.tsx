f77a57de4533801066200082d10ac08c7d6f52e6
gkochans <01162288@pw.edu.pl>
2025-01-09 22:43:28 +0100

    Poprawka paska dla panelu garażu

--- frontend/app/pages/[name]/garage-dashboard/page.tsx
+++ frontend/app/pages/[name]/garage-dashboard/page.tsx
@@ -44,7 +44,7 @@ export default function GarageDashboard() {
     return (
         <div className="absolute bottom-0 h-100 w-full">
             {/* Treść */}
-            <main className="flex items-center justify-center flex-col -mt-16 px-4 h-[calc(110vh-80px)]">
+            <main className="flex items-center justify-center flex-col -mt-16 px-4 h-[calc(100vh-80px)]">
                 <h2 className="text-4xl font-bold text-gray-800 mb-14">Panel Warsztatu</h2>
 
                 <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
